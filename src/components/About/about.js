377af03fc3008aa23225832db5fe77c40ee533d3
Kihun Kim <kihunkim@Kihuns-MBP.fritz.box>
2021-12-23 12:17:07 +0100

    about and profile page

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import {Icon, Button} from 'antd';
 import styled from "styled-components"
-import { useHistory } from 'react-router';
-
 
 const AboutWrapper = styled.div`
     padding-top:50px;
@@ -10,6 +8,8 @@ const AboutWrapper = styled.div`
     text-align:center;
     justify-content: center;
     font-size: 3rem;
+    padding-left:10vw;
+    padding-right:10vw;
 `;
 
 const AboutTitle = styled.h4``;
@@ -18,15 +18,17 @@ const AboutMessage = styled.p`
 `;
 
 export const About = () => {
-    const history = useHistory();
 
     return (
         <AboutWrapper>
             <AboutTitle>About "D3 & React Exhibition"</AboutTitle>
-            <AboutMessage>This page shows visualizing data in various forms in web browsers using javascript and React, HTML, CSS, and D3.
-            D3.js is a JavaScript library for manipulating documents based on data. D3 helps you bring data to life using HTML, SVG, and CSS. D3’s emphasis on web standards gives you the full capabilities of modern browsers without tying yourself to a proprietary framework, combining powerful visualization components and a data-driven approach to DOM manipulation.
-                <Icon type="smile" /></AboutMessage>
-            <Button onClick={() => history.push('/')}>Back to main page</Button>
+            <AboutMessage>This page shows beautiful data visualization in various forms in web browsers using javascript and React, HTML, CSS, and D3.
+            D3.js is a JavaScript library for manipulating documents based on data. D3 helps developers bring data to life using HTML, SVG, and CSS. 
+            D3’s emphasis on web standards gives you the full capabilities of modern browsers without tying yourself to a proprietary framework, combining 
+            powerful visualization components and a data-driven approach to DOM manipulation.
+                <Icon type="smile" /></AboutMessage>     
+
+                <Button onClick={() => window.location.assign("https://www.d3-graph-gallery.com/")}>Reference</Button>
         </AboutWrapper>
     )
 }
--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import {Icon, Button} from 'antd';
+import styled from "styled-components"
+import { MailOutlined,LinkedinOutlined,GithubOutlined } from "@ant-design/icons"
+
+const ProfileWrapper = styled.div`
+    padding-top:50px;
+    flex-direction:column;
+    text-align:center;
+    justify-content: center;
+    font-size: 3rem;
+    padding-left:10vw;
+    padding-right:10vw;
+`;
+
+
+const ProfileTitle = styled.h4``;
+const ProfileMessage = styled.p`
+  font-size:1rem;
+`;
+
+const LogoWrapper = styled.div`
+    flex-wrap:wrap;
+`;
 
 export const Profile = () => {
+
     return (
-        <div>
-           123 
-        </div>
+        <ProfileWrapper>
+            <ProfileTitle>Kihun Kim</ProfileTitle>
+            <ProfileMessage>This page is made by Kihun Kim. He is current frontend engineer in PACE Telematics in Germany. 
+                <Icon type="smile" /></ProfileMessage>
+            <Button onClick={() => window.location.assign('https://www.deutschkihun.com')}>Get to more about Kihun</Button>
+            <LogoWrapper>
+                <MailOutlined onClick={() => window.location.assign('mailto:deutschkihun@gmail.com')}/>
+                <LinkedinOutlined onClick={() => window.location.assign('https://www.linkedin.com/in/kihun-kim-b35b73174/')} />
+                <GithubOutlined onClick={() => window.location.assign('https://github.com/deutschkihun')} />
+            </LogoWrapper>
+        </ProfileWrapper>
     )
 }
 
